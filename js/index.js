6cdd273990f4735d2fbca9720d87e65bf0a1d4f7
Aliyu Kamilu <aliyukamilu002@gmail.com>
2022-09-28 09:09:34 +0100

    changes to header, image and some contents

---
+++ js/index.js
@@ -0,0 +1,60 @@
+$('#header-area').html(`
+<div class="dope-nav-container breakpoint-off">
+<div class="container">
+  <div class="row">
+    <!-- dope Menu -->
+    <nav class="dope-navbar justify-content-between" id="dopeNav">
+
+      <!-- Logo -->
+      <a class="nav-brand" href="index.html">
+        <img src="img/logo.png" alt="" width="50">
+      </a>
+
+      <!-- Navbar Toggler -->
+      <div class="dope-navbar-toggler">
+        <span class="navbarToggler">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+
+      <!-- Menu -->
+      <div class="dope-menu">
+
+        <!-- close btn -->
+        <div class="dopecloseIcon">
+          <div class="cross-wrap">
+            <span class="top"></span>
+            <span class="bottom"></span>
+          </div>
+        </div>
+
+        <!-- Nav Start -->
+        <div class="dopenav">
+          <ul id="nav">
+            <li>
+              <a href="#banner-section">Home</a>
+            </li>
+            <li>
+              <a href="#about-section">About</a>
+            </li>
+            <li>
+              <a href="#feature-section">Feature</a>
+            </li>
+            <li>
+              <a href="#testimonial-section">Testimonial</a>
+            </li>
+            <li>
+              <a href="#blog-section">Blog</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- Nav End -->
+      </div>
+    </nav>
+  </div>
+</div>
+</div>
+`)
